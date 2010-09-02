3086ed55eecfdf8b59e7cf8df67b12a16c9724a1
Buttacoli <buttacol@yahoo.com>
2010-09-01 22:48:40 -0500

    Compiles after major Socket2->Socket refactor, but not tested yet.

--- src/Socket.hpp
+++ src/Socket.hpp
@@ -1,67 +1,25 @@
-#ifndef INCLUDE_SOCKET_HPP
-#define INCLUDE_SOCKET_HPP
+#ifndef INCLUDED_SOCKET2_HPP
+#define INCLUDED_SOCKET2_HPP
 
-#include <unistd.h>
-#include <iostream>
+#include "Buffer.hpp"
 
-//! Wraps a Socket descriptor.
-//! The class acts as a proxy to the integer socket descriptor specifically
-//! so the destructor can be used to close the descriptor when the Socket 
-//! goes out of scope.  We can then not have to worry about closing the Socket
-//! if error's occur, it will do it by itself.
-class Socket {
+namespace ntee {
+
+class Socket2 {
 public:
+   
+   virtual ~Socket2() { /* empty */ }
+   
+   virtual int connectTo( Address& ) = 0;
+   virtual Socket2* listenOn( Address& ) = 0;
+   
+   virtual int send( const ntee::Buffer& ) = 0;
+   virtual Buffer* recv() = 0;
+   
+   virtual bool good() const = 0;
+   virtual int close() = 0;
+      
+};
 
-   
-   //! Constructor
-   Socket() : fd_(-1) {}
-   Socket(const std::string& name) : fd_(-1), name_(name) {}
-   
-   //! All important destructor.  This calls the close() function when the Socket
-   //! goes out of scope, so we can use instances as automatic variables.
-   ~Socket() { 
-      std::cerr << "close(" << fd_ << ")\n";
-      if ( fd_ > -1 ) close(fd_); 
-   }
-   
-   //! Assignment from integer.  This allows the Socket to be initialized directly
-   //! from the socket() function so it look natural.
-   //! @param v   Integer value to assign fd_
-   //! @returns Socket reference
-   Socket& operator=( int v ) { fd_ = v; return *this; }
-   
-   //! Cast to int.  This operator is called whenever a Socket instance is being
-   //! used in a situation where the integer would be expected (bind, listen, etc.)
-   //! This just makes the code look more natural.
-   operator int() const { return fd_; }
-   
-   //! Return the name of the Socket
-   std::string name() const;
-   int getDescriptor() const { return fd_; }
-   
-private:
-   // Unassignable and uncopyable?
-   Socket( const Socket& );
-   Socket& operator=( const Socket& );
-
-   int fd_;            //!< The descriptor  
-   std::string name_;  //!< an Identity
-};   
-
-
-//! Socket comparison to integer types.  This template function is instantiated 
-//! as the socket instance is compared to integer types.  This happens when 
-//! socket() functions are called in such a way: <p>
-//!    ErrIf( (sock=socket(...) == -1 ); <p>
-//! Here the returned Socket reference being assigned, is compared to -1.
-//!
-//! @param s    Socket reference on lhs
-//! @param v    Templated type (needs to be castable to int) on rhs
-//!
-//! @returns true if v == s.fd_ as an integer comparison. false otherwise.
-//!
-template <typename T> bool operator==( const Socket& s, const T& v ) {
-  return s.getDescriptor() == v;
-}
-
+} // end namespace ntee
 #endif
